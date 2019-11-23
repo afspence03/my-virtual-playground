2212ee216d9252076944e6ac898cda9f14defddd
afspence03 <afspence03@yahoo.com>
2019-11-22 21:07:26 -0600

    First project into github

---
+++ src/HelloWorld.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+
+    public HelloWorld () {
+        System.out.println("default constructor");
+    }
+
+    public HelloWorld(String in_string) {
+
+        System.out.println("Not the default constructor : Passed in string is : " + in_string);
+    }
+}
---
+++ src/main.java
@@ -0,0 +1,7 @@
+class main {
+public static void main(String[]args) {
+        HelloWorld helloWorld = new HelloWorld();
+
+        helloWorld = new HelloWorld("I'm an awesome String passed in");
+    }
+}
